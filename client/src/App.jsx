db8c0de62e9f1e0ba621540c3d7976bf289f1f1d
CERTIFIED2003 <111045472+CERTIFIED2003@users.noreply.github.com>
2023-04-17 13:53:50 +0530

    Created Sidebar & working on Navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,6 +13,8 @@ const App = () => {
 
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/create-campaign" element={<CreateCampaign />} />
         </Routes>
       </div>
     </div>
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { CustomButton } from "./";
+import { logo, menu, search, thirdweb } from "../assets";
+import { navlinks } from "../constants";
 
 const Navbar = () => {
+  const navigate = useNavigate();
+  const [isActive, setIsActive] = useState("dashboard");
+  const [toggleDrawer, setToggleDrawer] = useState(false);
+
   return (
-    <div>Navbar</div>
+    <div className="flex md:flex-row flex-col-reverse justify-between mb-[35px] gap-6">
+      <div className="lg:flex-1 flex flex-row max-w-[458px] py-2 pl-4 pr-2 h-[52px] bg-[#1C1C24] rounded-[100px] gap-3">
+        <input 
+          type="text"
+          placeholder="Search for campaigns"
+          className="flex w-full font-epilogue font-normal text-[14px] placeholder:text-[#4B5264] text-white bg-transparent outline-none"
+        />
+        <div className="w-[72px] h-full rounded-[20px] bg-[#4ACD8D] hover:bg-[#089752] flex justify-center items-center cursor-pointer">
+          <img
+            src={search}
+            alt="search"
+            className="w-[15px] h-[15px] object-contain"
+          />
+        </div>
+      </div>
+    </div>
   )
 }
 
